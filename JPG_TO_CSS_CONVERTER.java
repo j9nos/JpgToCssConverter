6fb1e232b4b3ac275a16394d8e6cbcc15d193c85
j9nos <olah.janos.gergely@gmail.com>
2022-08-31 15:30:30 +0200

    better time complexity

--- JPG_TO_CSS_CONVERTER.java
+++ JPG_TO_CSS_CONVERTER.java
@@ -17,9 +17,10 @@ public class JPG_TO_CSS_CONVERTER {
         String outputHTML = "index.html";
 
         try {
+
             loadImage(inputJPG);
-            List<Color> pixels = harvestPixels();
-            writeIntoHTML(pixels, outputHTML);
+            writeIntoHTML(outputHTML);
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -30,36 +31,18 @@ public class JPG_TO_CSS_CONVERTER {
         bufferedImage = ImageIO.read(new File(inputJPG));
     }
 
-    static List<Color> harvestPixels() {
-        List<Color> pixels = new ArrayList<>();
-        for (int x = 0; x < bufferedImage.getWidth(); ++x) {
-            for (int y = 0; y < bufferedImage.getHeight(); ++y) {
-                pixels.add(new Color(bufferedImage.getRGB(x, y), false));
-            }
-        }
-        return pixels;
-    }
-
-    static void writeIntoHTML(List<Color> pixels, String outputHTML) throws IOException {
+    static void writeIntoHTML(String outputHTML) throws IOException {
         FileWriter file = new FileWriter(outputHTML);
         file.write("<style>\n\t#drawing {\n\t\tposition:absolute;\n\t\tbox-shadow:\n");
-        int currentPixelCounter,x,y;
-        currentPixelCounter=x=y=0;
-        char commaOrSemicolon = ',';
-        for (Color pixel : pixels) {
-            if (x == bufferedImage.getWidth()) {
-                x = 0;
-                y++;
+        int i=0;
+        for (int x = 0; x < bufferedImage.getWidth(); ++x) {
+            for (int y = 0; y < bufferedImage.getHeight(); ++y) {
+                Color currentPixel = new Color(bufferedImage.getRGB(x, y), false);
+                char commaOrSemicolon = i+1 == bufferedImage.getWidth()*bufferedImage.getHeight() ? ';' : ',';
+                file.write(String.format("\t\t\t%dpx %dpx 1px 1px rgb(%d, %d, %d)%c\n", x, y, currentPixel.getRed(),
+                        currentPixel.getGreen(), currentPixel.getBlue(), commaOrSemicolon));
+                        i++;
             }
-            if (currentPixelCounter + 1 == pixels.size()) {
-                commaOrSemicolon = ';';
-            }
-            int r = pixel.getRed();
-            int g = pixel.getGreen();
-            int b = pixel.getBlue();
-            file.write(String.format("\t\t\t%dpx %dpx 1px 1px rgb(%d, %d, %d)%c\n", y, x, r, g, b, commaOrSemicolon));
-            x++;
-            currentPixelCounter++;
         }
         file.write("\t}\n</style>\n<body>\n\t<div id='drawing'>\n</div>\n</body>");
         file.close();
